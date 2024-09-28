339527b09bf05493da1924809ecc6a186893e59b
Isaiah Houssou <82531560+musketsgithub@users.noreply.github.com>
2024-09-28 11:35:58 -0400

    more strategiers

--- trader/support_resistance_breakout.py
+++ trader/support_resistance_breakout.py
@@ -6,9 +6,10 @@ import pandas_ta as ta
 import matplotlib.pyplot as plt
 
 # Define the main function
-def support_and_resistance_rejection(ohlcv: pd.DataFrame, lookback: int = 240, atr_lookback: int = 168, hold_period = 48,
+def support_and_resistance_rejection(ohlcv: pd.DataFrame, lookback: int = 240, atr_lookback: int = 168, hold_period = 48, confirmation_wait:int=12,
                                      tp_mult : float=3.0, sl_mult : float=3.0, plot_graphs : bool = False, sigma : float = 0.0025,
-                                     min_bounces : int = 2, zone_size : float = 0.002, rsi_lookback : int = 48, rsi_min : float = 0):
+                                     min_bounces : int = 2, zone_size : float = 0.002, rsi_lookback : int = 48, rsi_min : float = 0,
+                                     confirmation_mult: float=1.0, atr_min : float = 0.001):
     ohlcv = ohlcv.reset_index()  # Reset the index
 
     numeric_cols = ohlcv.select_dtypes(include=[np.number]).columns
@@ -19,6 +20,8 @@ def support_and_resistance_rejection(ohlcv: pd.DataFrame, lookback: int = 240, a
     # Calculate ATR
     atr_arr = ta.atr(ohlcv['high'], ohlcv['low'],ohlcv['close'], atr_lookback)
     atr_arr = atr_arr.to_numpy()
+
+    np.set_printoptions(threshold=np.inf)
 
     rsi_atr = ta.rsi(ohlcv['close'], rsi_lookback)
     rsi_atr = rsi_atr.to_numpy()
@@ -36,7 +39,10 @@ def support_and_resistance_rejection(ohlcv: pd.DataFrame, lookback: int = 240, a
     # ohlcv_modified = ohlcv.copy()
     # ohlcv_modified['high'] = ohlcv_modified['close']
     # ohlcv_modified['low'] = ohlcv_modified['close']
-    all_extremes = directional_change.get_extremes(ohlcv).extremes
+    extremes_df = directional_change.get_extremes(ohlcv)
+    all_extremes = extremes_df.extremes
+
+    print(extremes_df.extremes)
 
     zone_bounds_plotting = {}
     extremes_in_zone_plotting = {}
@@ -50,7 +56,7 @@ def support_and_resistance_rejection(ohlcv: pd.DataFrame, lookback: int = 240, a
 
         count_extremes_in_zone = len(extremes_in_zone)
 
-        if not in_trade and count_extremes_in_zone >= min_bounces:
+        if not in_trade and count_extremes_in_zone >= min_bounces and atr_arr[i] > atr_min:
             #1 is from top, 0 is from bottom
             entry_direction = None
             for j in range(i, i - lookback, -1):
@@ -60,42 +66,46 @@ def support_and_resistance_rejection(ohlcv: pd.DataFrame, lookback: int = 240, a
                 elif close[j] < current_zone[0]:
                     entry_direction = 0
                     break
-            #
-            # if (entry_direction==1 and rsi_atr[i] < 50+rsi_min) or (entry_direction==0 and rsi_atr[i] > 50-rsi_min):
-            #     continue
 
-            if entry_direction == 1:
-                tp_price = close[i] + atr_arr[i] * tp_mult
-                sl_price = close[i] - atr_arr[i] * sl_mult
-            elif entry_direction == 0:
-                tp_price = close[i] - atr_arr[i] * tp_mult
-                sl_price = close[i] + atr_arr[i] * sl_mult
-
-            hp_i = i + hold_period
             in_trade = True
 
-            trades.loc[trade_i, 'entry_i'] = i
-            trades.loc[trade_i, 'entry_p'] = close[i]
-            trades.loc[trade_i, 'atr'] = atr_arr[i]
-            trades.loc[trade_i, 'sl'] = sl_price
-            trades.loc[trade_i, 'tp'] = tp_price
-            trades.loc[trade_i, 'hp_i'] = i + hold_period
+            if entry_direction==1:
+                trades.loc[trade_i, 'confirmation'] = close[i] + confirmation_mult * atr_arr[i]
+            elif entry_direction==0:
+                trades.loc[trade_i, 'confirmation'] = close[i] - confirmation_mult * atr_arr[i]
+            trades.loc[trade_i, 'confirmation_hp_i'] = i + confirmation_wait
             trades.loc[trade_i, 'type'] = entry_direction
+            trades.loc[trade_i, 'potential'] = True
 
             zone_bounds_plotting[trade_i] = current_zone
 
             extremes_in_zone_plotting[trade_i] = (extremes_in_zone.index.tolist(), extremes_in_zone['extreme'].tolist())
 
-        if in_trade:
-            if (trades.loc[trade_i, 'type'] == 1 and (close[i] >= tp_price or close[i] <= sl_price)) or \
-                    (trades.loc[trade_i, 'type'] == 0 and (close[i] <= tp_price or close[i] >= sl_price)) or \
-                    (i >= hp_i):
+        if in_trade and trades.loc[trade_i, 'potential'] == True:
+            if (trades.loc[trade_i, 'type']==1 and (close[i] >= trades.loc[trade_i, 'confirmation'])) or \
+                    (trades.loc[trade_i, 'type']==0 and (close[i] <= trades.loc[trade_i, 'confirmation'])):
+                trades.loc[trade_i, 'potential'] = False
+                trades.loc[trade_i, 'sl'] = close[i] - atr_arr[i] * sl_mult if trades.loc[trade_i, 'type']==1 else close[i] + atr_arr[i]*sl_mult
+                trades.loc[trade_i, 'tp'] = close[i] + atr_arr[i] * tp_mult if trades.loc[trade_i, 'type']==1 else close[i] - atr_arr[i]*tp_mult
+                trades.loc[trade_i, 'entry_i'] = i
+                trades.loc[trade_i, 'entry_p'] = close[i]
+                trades.loc[trade_i, 'hp_i'] = i + hold_period
+            elif i >= trades.loc[trade_i, 'confirmation_hp_i']:
+                in_trade = False
+                trade_i += 1
+
+        if in_trade and trades.loc[trade_i, 'potential'] == False:
+            if (trades.loc[trade_i, 'type'] == 1 and (close[i] >= trades.loc[trade_i, 'tp'] or close[i] <= trades.loc[trade_i, 'sl'])) or \
+                    (trades.loc[trade_i, 'type'] == 0 and (close[i] <= trades.loc[trade_i, 'tp'] or close[i] >= trades.loc[trade_i, 'sl'])) or \
+                    (i >= trades.loc[trade_i, 'hp_i']):
                 trades.loc[trade_i, 'exit_i'] = i
                 trades.loc[trade_i, 'exit_p'] = close[i]
 
                 if plot_graphs:
                     # Plotting the results
                     plt.figure(figsize=(14, 7))
+
+                    print(trades.loc[trade_i, 'entry_p'], trades.loc[trade_i, 'exit_p'])
 
                     if (trades.loc[trade_i, 'exit_p'] - trades.loc[trade_i, 'entry_p'] > 0 and trades.loc[trade_i, 'type'] == 1) or \
                             (trades.loc[trade_i, 'entry_p'] - trades.loc[trade_i, 'exit_p'] > 0 and trades.loc[trade_i, 'type'] == 0):
@@ -142,6 +152,8 @@ def support_and_resistance_rejection(ohlcv: pd.DataFrame, lookback: int = 240, a
                 in_trade = False
                 trade_i += 1
 
+    trades = trades.dropna()
+
     if len(trades) > 0:
         trades['return'] = trades.apply(
             lambda row: row['exit_p'] - row['entry_p'] if row['type'] == 1
@@ -149,29 +161,31 @@ def support_and_resistance_rejection(ohlcv: pd.DataFrame, lookback: int = 240, a
             axis=1
         )
 
+    print(trades['return'])
+
     return trades
 
 if __name__ == "__main__":
     instruments = ["C:EURUSD", "C:EURJPY", "C:EURGBP", "C:GBPUSD", "C:AUDCAD"]
-    instruments  = ["C:EURUSD"]
+    # instruments = ["C:EURUSD", "C:EURJPY", "C:EURGBP", "C:AUDCAD", "C:AUDCHF"]
 
-    instrument = "C:EURUSD"
+    instrument = "C:USDJPY"
 
-    start = '2014-09-18'
+    start = '2023-09-18'
     end = '2024-09-18'
     granularity = ['1', 'hour']
 
     # total_win_count = 0
     # total_trade_count = 0
     # all_profit = 0
-    #
+
     # for instrument in instruments:
     #     print("Fetching data...")
     #     api = PolygonAPI()
     #     df = api.get_data([instrument], start, end, granularity)[instrument]
     #
     #     print("Running strategy...")
-    #     trades = support_and_resistance_rejection(df, plot_graphs=True, sigma=0.0025, min_bounces=2, zone_size = 0.002, rsi_min=5)
+    #     trades = support_and_resistance_rejection(df, plot_graphs=False, sigma=0.0025, min_bounces=2, zone_size = 0.002, confirmation_mult = 3.0)
     #
     #     # Calculate total profit
     #     total_profit = trades['return'].sum() if len(trades)>0 else 0
@@ -187,26 +201,27 @@ if __name__ == "__main__":
     #     all_profit += total_profit
     #
     # win_rate = (total_win_count / total_trade_count) * 100 if total_trade_count > 0 else 0
-    #
+    # #
     print("Fetching data...")
     api = PolygonAPI()
     df = api.get_data([instrument], start, end, granularity)[instrument]
 
-    for sigma in [0.001, 0.0025, 0.005, 0.01]:
-        print("Running strategy...")
-        trades = support_and_resistance_rejection(df, plot_graphs=False, sigma=sigma, min_bounces=2, zone_size = 0.002, rsi_min=5)
+    print("Running strategy...")
+    trades = support_and_resistance_rejection(df, plot_graphs=False, sigma=0.0025, min_bounces=2, zone_size = 0.002, confirmation_mult=2.0, atr_min=0.001)
 
-        # Calculate total profit
-        total_profit = trades['return'].sum() if len(trades)>0 else 0
+    # Calculate total profit
+    total_profit = trades['return'].sum() if len(trades)>0 else 0
 
-        # Calculate win rate
-        win_count = (trades['return'] > 0).sum() if len(trades)>0 else 0
-        total_trades = len(trades)
-        win_rate = (win_count / total_trades) * 100 if total_trades > 0 else 0
+    # Calculate win rate
+    win_count = (trades['return'] > 0).sum() if len(trades)>0 else 0
+    total_trades = len(trades)
+    win_rate = (win_count / total_trades) * 100 if total_trades > 0 else 0
 
-        print(f"Win Rate: {win_rate}")
+    print(f"Total Profit {total_profit}")
+    print(f"Win Rate: {win_rate}")
+    print(f"Total Trades: {total_trades}")
 
-    # # Print the results
+    # Print the results
     # print(f'Total Profit: {all_profit}')
     # print(f'Win Rate: {win_rate:.2f}%')
     # print(f'Total Trade Count: {total_trade_count}')
