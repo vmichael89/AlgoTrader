e6cb10fd3cef5b42aca66945d15dbc2089f03bf0
Vyndell Michael <vmichael@gmail.com>
2024-10-01 14:15:13 +0200

    [feat] strat_s/d added plot with error todo

--- scratch_files/support_demand.py
+++ scratch_files/support_demand.py
@@ -38,24 +38,57 @@ fig = go.Figure(data=[go.Candlestick(x=df.index, open=df.open, high=df.high, low
 fig.update_layout(title=str(trader.data[0]), xaxis_rangeslider_visible=False)
 fig.add_scatter(x=dc.extremes.index, y=dc.extremes.extreme)
 
-# Add annotations for higher bottom counts
-for i, row in df[df['type'] == 'bottom'].iterrows():
-    fig.add_annotation(
-        x=i,
-        y=row['extreme'],
-        text=f"{int(row['higher_bottom_count'])}",
-        showarrow=False,
-        bgcolor="lightblue"
-    )
+for timestamp, row in df[['type', 'higher_bottom_count', 'higher_top_count']].dropna().iterrows():
+    last_extremes_gr_thresh = [None, None]
+    type_names = ('bottom', 'top')
+    count_names = ['higher_bottom_count', 'higher_top_count']
+    for itype in range(2):
+        if row['type'] == type_names[itype]:
+            if row[count_names[itype]] >= 3:
+                last_extremes_gr_thresh[itype] = timestamp
 
-# Add annotations for higher top counts
-for i, row in df[df['type'] == 'top'].iterrows():
-    fig.add_annotation(
-        x=i,
-        y=row['extreme'],
-        text=f"{int(row['higher_top_count'])}",
-        showarrow=False,
-        bgcolor="lightgreen"
-    )
+    if (row[count_names] >= 3).all():
+        fig.add_annotation(
+            x=last_extremes_gr_thresh[0],
+            # TODO ERROR here
+            #  because last_extremes_gr_thresh is [None, Timestamp('2024-08-21 18:15:00+0000', tz='UTC')]
+            #  but row is
+            #  type                   top
+            #  higher_bottom_count    6.0   <- last_extremes_gr_thresh[0] must have been set
+            #  higher_top_count       3.0
+            y=df.loc[last_extremes_gr_thresh[0], 'extreme'],
+            text=f"{int(row['higher_bottom_count'])}",
+            showarrow=False,
+            bgcolor="lightblue"
+        )
+        fig.add_annotation(
+            x=last_extremes_gr_thresh[1],
+            y=df.loc[last_extremes_gr_thresh[1], 'extreme'],
+            text=f"{int(row['higher_top_count'])}",
+            showarrow=False,
+            bgcolor="lightgreen"
+        )
+
+
+
+# # Add annotations for higher bottom counts
+# for i, row in df[df['type'] == 'bottom'].iterrows():
+#     fig.add_annotation(
+#         x=i,
+#         y=row['extreme'],
+#         text=f"{int(row['higher_bottom_count'])}",
+#         showarrow=False,
+#         bgcolor="lightblue"
+#     )
+#
+# # Add annotations for higher top counts
+# for i, row in df[df['type'] == 'top'].iterrows():
+#     fig.add_annotation(
+#         x=i,
+#         y=row['extreme'],
+#         text=f"{int(row['higher_top_count'])}",
+#         showarrow=False,
+#         bgcolor="lightgreen"
+#     )
 
 fig.show(renderer='browser')
