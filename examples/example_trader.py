9997bfcedf75f711ac26dc7f25fad8e1834661cb
Vyndell Michael <vmichael@gmail.com>
2024-10-07 11:32:45 +0200

    [feat] trader - added plot example / updated trader example with saving

--- examples/example_trader.py
+++ examples/example_trader.py
@@ -3,4 +3,5 @@ os.chdir("../trader")
 from trader.trader import Trader
 
 trader = Trader()
-trader.add_data(['EUR_USD'])
+trader.add_data(['EUR_USD','USD_JPY'], price="BA")
+trader.save_data(save_all=True)
